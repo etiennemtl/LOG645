0d40ef321792ef6e25c38a19289dc96006443c1c
Etienne Soulard-Geoffrion <esoulardgeoffrion@gmail.com>
2016-10-26 16:05:21 -0400

    Finishes sequential part

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -37,28 +37,27 @@ int main(int argc, char* argv[])
     if (id == 0) {
         int i, j, k;
         
-        double*** matrix = (double ***) malloc(n * sizeof(double**));
-        for (i = 0; i < m; i++) {
-            matrix[i] = (double **) malloc(m * sizeof(double*));
-            for (j = 0; j < n; j++) {
-                matrix[i][j] = (double *) malloc(np * sizeof(double));
+        double*** matrix = (double ***) malloc((np+1) * sizeof(double**));
+        for (k = 0; k <= np; k++) {
+            matrix[k] = (double **) malloc(m * sizeof(double*));
+            for (i = 0; i < m; i++) {
+                matrix[k][i] = (double *) malloc(n * sizeof(double));
             }
         }
         
         for (i = 0; i < m; i++) {
             for (j = 0; j < n; j++) {
-                matrix[i][j][0] = (double) i*(m-i-1) * j*(n-j-1);
+                matrix[0][i][j] = (double) i*(m-i-1) * j*(n-j-1);
             }
         }
         
         double a = 1.0 - (4.0*td)/(h*h);
         double b = td/(h*h);
         
-        
-        for (i = 1; i < m - 1 ; i++) {
-            for (j = 1; j < n - 1; j++) {
-                for (k = 0; k < np; k++) {
-                    matrix[i][j][k+1] = (double) ( ( a * matrix[i][j][k] ) + ( b * (matrix[i-1][j][k] + matrix[i+1][j][k] + matrix[i][j-1][k] + matrix[i][j+1][k]) ) );
+        for (k = 1; k <= np; k++) {
+            for (i = 1; i < m - 1 ; i++) {
+                for (j = 1; j < n - 1; j++) {
+                    matrix[k][i][j] = (double) ( ( a * matrix[k-1][i][j] ) + ( b * (matrix[k-1][i-1][j] + matrix[k-1][i+1][j] + matrix[k-1][i][j-1] + matrix[k-1][i][j+1]) ) );
                 }
             }
         }
@@ -75,8 +74,9 @@ void printMatrix(double*** matrix, int n, int m, int np)
     int i, j;
     for (i = 0; i < m ; i++) {
         for (j = 0; j < n; j++) {
-            printf("%d\t",(int) matrix[i][j][np]);
+            printf("%d\t",(int) matrix[np][i][j]);
         }
+        printf("\n");
         printf("\n");
     }
 }
